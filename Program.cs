4ca868de7aafb6088e2c2b8015e96f066312c1a1
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 13:17:19 +0100

    Ex 03 Tableaux Listes Enum

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabEx03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche" };
+            for (int i = 0; i < week.Length; i++)
+            {
+                week[4] = "Vendredi";
+                Console.WriteLine(week[i]);
+
+            }
+        }
+    }
+}
